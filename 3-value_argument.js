0af88aeaba591a749935adfeb5da84c70151e70a
Enoch Baah <ebaah2942@gmail.com>
2025-07-10 20:23:43 +0000

    Correections

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -3,7 +3,7 @@ const args = process.argv.slice(2);
 if (!args[0]) {
   console.log("No argument");
 } else if (!args[1]) {
-  console.log("Argument found");
+  console.log("HBTN");
 } else {
-  console.log("Arguments found");
+  console.log("HBTN cool");
 }
