021b1899c46f77271f461f2c8b6e1494fc1c7cd3
Enoch Baah <ebaah2942@gmail.com>
2025-07-10 20:10:00 +0000

    new checks

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,8 +1,3 @@
-/*If no arguments are passed to the script, print “No argument”
-You must use console.log(...) to print all output
-You are not allowed to use var
-You are not allowed to use length*/
-
 const args = process.argv.slice(2);
 
 if (!args[0]) {
